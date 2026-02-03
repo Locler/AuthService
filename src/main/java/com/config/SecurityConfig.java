c8e1299a8a472da49105a89fa44b2d09cf8a6229
Locler <spartak1469@gmail.com>
2026-02-03 17:23:48 +0300

    App prop + k8s changes

--- src/main/java/com/config/SecurityConfig.java
+++ src/main/java/com/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**","/actuator/health/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
